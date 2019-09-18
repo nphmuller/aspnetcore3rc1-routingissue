ff6b62854d011b01ec30e511b6c9b809094f052e
Nick Muller <muller_nicky@hotmail.com>
2019-09-18 15:00:12 +0200

    ASP.NET Core 2.2 - http://localhost:5000/test/hi returns 200 with `hi`.

---
+++ WebApplication30/CustomEndpoints.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication30
+{
+    public class CustomEndpoints : ControllerBase
+    {
+        [HttpGet]
+        public Task<string> HiAsync()
+        {
+            return Task.FromResult("Hi!");
+        }
+    }
+}
---
+++ WebApplication30/Startup.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebApplication30
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute("test_route", "test/hi",
+                    new
+                    {
+                        Controller = nameof(CustomEndpoints),
+                        Action = nameof(CustomEndpoints.HiAsync)
+                    }
+                );
+            });
+        }
+    }
+}
